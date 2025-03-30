586c8887c6981d6960386d0371fe29cb9d848266
sxdas <am8520996@gmail.com>
2025-03-31 00:36:18 +0300

    Алгоритмы и структуры данных.Очередь

---
+++ очередь.py
@@ -0,0 +1,29 @@
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if self.is_empty():
+            raise ValueError('Очередь пустая')
+        return self.queue.pop(0)
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def size(self):
+        return len(self.queue)
+
+queue = Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+
+print("Размер очереди:", queue.size())  # Размер очереди: 3
+
+while not queue.is_empty():
+    item = queue.dequeue()
+    print("Извлечен элемент:", item)
